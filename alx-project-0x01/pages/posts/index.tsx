3af94d1da13401a1b56796d8aa5b553a4cf19df4
whizikem <wisdomikem@gmail.com>
2025-07-19 17:47:32 +0100

    Setting up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -4,10 +4,12 @@
 
 import React from "react"
 import PostCard from "@/components/common/PostCard"
+import Header from "@/components/layout/Header";
 
 const Posts: React.FC = () => {
   return (
     <div>
+      <Header />
       <h1>Posts</h1>
       <PostCard />
     </div>
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,12 +2,14 @@
  * Users component
  */
 
-import Button from "@/components/common/Button";
 import React from "react";
+import Button from "@/components/common/Button";
+import Header from "@/components/layout/Header";
 
 const Users: React.FC = () => {
   return (
     <div>
+      <Header />
       <h1>Users</h1>
       <Button />
     </div>
